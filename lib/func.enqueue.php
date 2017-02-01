e8852fb2758a3e3545003f831a1708d828d8a90e
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-02-01 19:54:34 +0100

    Changement préfixes des fonctions

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -8,18 +8,21 @@
 **************************/
 
 //* enqueue style front
-add_action('wp_enqueue_scripts','gn_styles_front', 99);
-function gn_styles_front() {
+add_action('wp_enqueue_scripts','text_rotator_styles_front', 99);
+function text_rotator_styles_front() {
 	wp_register_style('styles-text-rotator', plugins_url( 'css/simpletextrotator.css', dirname(__FILE__) ), array(), '1.0', 'all' );
+	wp_register_style('styles-projet-front', plugins_url( 'css/styles-projet-front.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	if ( is_home() ) {
 		wp_enqueue_style('styles-text-rotator');
+		wp_enqueue_style('styles-projet-front');
+
 	}
-	
+
 }
 
 //* enqueue script front
-add_action('wp_enqueue_scripts','gn_scripts_front');
-function gn_scripts_front() {
+add_action('wp_enqueue_scripts','text_rotator_scripts_front');
+function text_rotator_scripts_front() {
 	wp_register_script('scripts-text-rotator',plugins_url( 'js/jquery.simple-text-rotator.min.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	if ( is_home() ) {
 		wp_enqueue_script('scripts-text-rotator');
@@ -31,14 +34,14 @@ function gn_scripts_front() {
 **************************/
 
 //* enqueue style back
-// add_action('admin_enqueue_scripts','gn_styles_back', 99);
-function gn_styles_back() {
+// add_action('admin_enqueue_scripts','text_rotator_styles_back', 99);
+function text_rotator_styles_back() {
 	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-back');
 }
 
 //* enqueue script back
-// add_action('admin_enqueue_scripts','gn_scripts_back');
+// add_action('admin_enqueue_scripts','text_rotator_scripts_back');
 function gn_scripts_back() {
 	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-back');
--- lib/func.wordpress.php
+++ lib/func.wordpress.php
@@ -5,18 +5,18 @@
 // show_admin_bar( false );
 
 // Ajouter script Text Rotator en bas de la page
-add_action( 'wp_footer', 'ap_script_rotator', 999 );
-function ap_script_rotator() {
+add_action( 'wp_footer', 'text_rotator_script', 999 );
+function text_rotator_script() {
 	if ( ! is_home() ) {
 		return;
-	}0 
+	}0
 ?>
 	<script type="text/javascript">
 		jQuery(".rotate").textrotator({
 		  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
 		  separator: "*", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
 		  speed: 2000 // How many milliseconds until the next word show.
-		});			
+		});
 	</script>
 
 
