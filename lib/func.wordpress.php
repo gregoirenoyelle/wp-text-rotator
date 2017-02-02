4b045d96d072b4496d51960385bd6071b341d3d1
Grégoire Noyelle <hello@gregoirenoyelle.com>
2017-02-02 17:30:16 +0100

    Intégration de commentaire dans le scripts

--- lib/func.wordpress.php
+++ lib/func.wordpress.php
@@ -1,16 +1,17 @@
 <?php
-//* Fonction pour WordPress
+//* Intégration du script dans mon modèle
 
-//Enlever la barre Admin en front
-// show_admin_bar( false );
 
 // Ajouter script Text Rotator en bas de la page
 add_action( 'wp_footer', 'text_rotator_script', 999 );
 function text_rotator_script() {
+    // Condition pour voir si je suis sur la home
+    // Dans le cas contraire, je stoppe l'action avec return
 	if ( ! is_home() ) {
 		return;
-	}0
+	}
 ?>
+    <!-- Script pour initialiser text rotator -->
 	<script type="text/javascript">
 		jQuery(".rotate").textrotator({
 		  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
